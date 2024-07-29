1cc13dedaaae275b952661e26847c746ea112ff2
Nicolas Eiriz <neiriz@sds_domain_1.local>
2024-07-29 11:38:27 -0300

    agrego ejercicio 1 del desafio 5

---
+++ desafios/tarea-5/main.js
@@ -0,0 +1,22 @@
+// Ejercicio 1
+
+let cantidadDeGatos = 12
+
+for(let i = 1; i <= cantidadDeGatos; i++){
+    if(i % 3 === 1){
+        console.log(`Gato #${i}: 😺`)
+    }
+
+    else if(i % 3 === 2){
+        console.log(`Gato #${i}: 😸`)
+    }
+
+    else if(i % 3 === 0){
+        console.log(`Gato #${i}: 😹`)
+    }
+}
+
+
+// Ejercicio 2
+
+let cantidadDeGatos

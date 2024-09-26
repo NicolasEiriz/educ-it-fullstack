6209462ea8b602c77bb22c2539d1661d3c3eba3b
Nicolas Eiriz <neiriz@sds_domain_1.local>
2024-09-26 13:30:28 -0300

    agrego main

---
+++ desafios/ejercicio-08-pedidos-en-linea/main.js
@@ -0,0 +1,11 @@
+/*Pasos para resolver el ejercicio:
+
+1.Definir los objetos para representar los artículos del menú con sus atributos, como nombre, descripción y precio.
+2.Crear una lista de artículos del menú disponibles para seleccionar.
+3.Capturar los eventos de selección del usuario y almacenar la información del pedido en variables.
+4.Validar los campos de entrada del formulario antes de enviar el pedido.
+5.Calcular el costo total del pedido.
+6.Enviar el pedido al restaurante.
+7.Mostrar una confirmación del pedido con los detalles del pedido y el costo total.*/
+
+
